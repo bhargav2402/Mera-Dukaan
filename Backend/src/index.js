a4956090dd4b6c463c1999ef3066b94afaa502e2
Vignesh-604 <vig6604@gmail.com>
2024-08-17 21:20:19 +0530

    authentication needs fixing. added passport, jwt and cookie parser

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,10 +2,9 @@ import dotenv from "dotenv"
 import cors from "cors"
 import { connect } from "mongoose";
 import express from "express"
-// import cookieparser from "cookieparser"
-
-import Customer from "./models/costumer.model.js";
-
+import passport from "./middlewares/passport.middleware.js";
+import session from "express-session";
+import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: "./env" })
 
@@ -16,7 +15,18 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"));
-// app.use(cookieparser())
+app.use(cookieParser())
+
+// Express session middleware (required by Passport.js)
+app.use(session({
+    secret: 'your-secret-key', // Change this to a secure key
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Initialize Passport.js and session
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Database connection
 (async () => {
